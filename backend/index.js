831aac442b41f68369c56894a93b0c88ebfc7525
Yohanes Febryan Kana Nyola <151604647+ryankananyola@users.noreply.github.com>
2025-06-10 15:58:55 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,22 +8,26 @@ dotenv.config();
 
 const app = express();
 
-app.use((err, req, res, next) => {
-  console.error('Error:', err);
-  res.status(500).json({ error: err.message });
-});
-
-// Menggunakan CORS middleware
-app.use(cors({
-  origin: 'https://inventarisasi-fe-dot-b-05-450916.uc.r.appspot.com',  // Ganti dengan URL tempat frontend berjalan
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+// Middleware CORS
+const corsOptions = {
+  origin: ['https://inventarisasi-fe-dot-b-05-450916.uc.r.appspot.com'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
-}));
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); // Tangani preflight request (OPTIONS)
 
 app.use(express.json());
 app.use(UserRoute);
 app.use(BarangRoute);
+
+// Middleware Error Handler (letakkan paling bawah setelah route)
+app.use((err, req, res, next) => {
+  console.error('Error:', err);
+  res.status(500).json({ error: err.message });
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
