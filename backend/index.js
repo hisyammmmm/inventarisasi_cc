e16952685a93bf7e3d59a6231f7403892e8ed5f5
hisyammmmm <ibamhisyam@gmail.com>
2025-06-10 13:28:04 +0700

    lagi

--- backend/index.js
+++ backend/index.js
@@ -24,6 +24,7 @@ app.use(express.json());
 app.use(UserRoute);
 app.use(BarangRoute);
 
-app.listen(5000, () => {
-  console.log('Server running on port 5000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
