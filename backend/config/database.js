492211b188e13c0cef20ec2cd3b4af6052122bb0
hisyammmmm <ibamhisyam@gmail.com>
2025-06-10 13:58:09 +0700

    ada log

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,6 +3,13 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+console.log("Connecting to DB with:", {
+  DB_NAME: process.env.DB_NAME,
+  DB_USERNAME: process.env.DB_USERNAME,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_HOST: process.env.DB_HOST,
+});
+
 export const mysqlDb = new Sequelize(
   process.env.MYSQL_DB_NAME,
   process.env.MYSQL_DB_USERNAME,
